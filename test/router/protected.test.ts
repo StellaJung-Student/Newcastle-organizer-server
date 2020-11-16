22c22d2a12d323e53630c5cbcb3c5dd3ad992745
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-11-17 00:33:26 +0100

    Add passport jwt, google, local strategy

--- test/router/protected.test.ts
+++ test/router/protected.test.ts
@@ -1,6 +1,19 @@
 import request from 'supertest';
 import app from '../../server/app';
 
+describe('Test IndexController', () => {
+  it('Request / should return Hello!', async () => {
+    const result = await request(app).get('/').send();
+
+    expect(result.status).toBe(200);
+    expect(result.body.data).toBe('Hello!');
+  });
+});
+
+/*
+import request from 'supertest';
+import app from '../../server/app';
+
 describe('Test for passport', () => {
   let token: string = null;
 
@@ -36,3 +49,4 @@ describe('Test for passport', () => {
     expect(response.status).toBe(200);
   });
 });
+*/

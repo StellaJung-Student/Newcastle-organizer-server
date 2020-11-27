c7a7675e70598e6fc45b4034e1f11bbb09f6fdad
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-11-27 15:18:07 +0100

    cors

--- server/app.ts
+++ server/app.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['https://newcastle-organizer.vercel.app/', 'http://localhost:3000'],
+    origin: ['https://newcastle-organizer.vercel.app', 'http://localhost:3000'],
     credentials: true,
   })
 );

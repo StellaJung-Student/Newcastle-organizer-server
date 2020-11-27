475cae10d3b4276646b2a785920cdb9f68b632f7
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-11-27 15:10:30 +0100

    cors

--- server/server.ts
+++ server/server.ts
@@ -14,9 +14,9 @@ createConnection({
   database: AWS_RDS_INIT_DB,
   synchronize: true,
   logging: true,
-  entities: ['server/models/**/*.*'],
-  migrations: ['server/migration/**/*.*'],
-  subscribers: ['server/subscriber/**/*.*'],
+  entities: ['dist/models/**/*.*'],
+  migrations: ['dist/migration/**/*.*'],
+  subscribers: ['dist/subscriber/**/*.*'],
   cli: {
     entitiesDir: 'dist/models',
     migrationsDir: 'dist/migration',

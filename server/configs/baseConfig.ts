55e5beda5397397f91e4fa6007b775395c3aeae9
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-11-27 14:09:24 +0100

    Change database connection

--- server/configs/baseConfig.ts
+++ server/configs/baseConfig.ts
@@ -3,3 +3,7 @@ export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const BASE_API_URL = process.env.BASE_API_URL || 'http://localhost:3000';
+export const AWS_RDS_HOST = process.env.AWS_RDS_HOST;
+export const AWS_RDS_USERNAME = process.env.AWS_RDS_USERNAME;
+export const AWS_RDS_PASSWORD = process.env.AWS_RDS_PASSWORD;
+export const AWS_RDS_INIT_DB = process.env.AWS_RDS_INIT_DB;

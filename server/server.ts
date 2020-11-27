7a98bab0f9f2ab50e70fbfac653fba07dc409748
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-11-27 14:09:24 +0100

    Change database connection

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,26 @@ import './security/passport';
 import app from './app';
 import { createConnection } from 'typeorm';
 
-import { PORT } from './configs/baseConfig';
+import { AWS_RDS_HOST, AWS_RDS_INIT_DB, AWS_RDS_PASSWORD, AWS_RDS_USERNAME, PORT } from './configs/baseConfig';
 
-createConnection(/*...*/)
+createConnection({
+  type: 'postgres',
+  host: AWS_RDS_HOST,
+  port: 5432,
+  username: AWS_RDS_USERNAME,
+  password: AWS_RDS_PASSWORD,
+  database: AWS_RDS_INIT_DB,
+  synchronize: true,
+  logging: true,
+  entities: ['dist/models/**/*.*'],
+  migrations: ['dist/migration/**/*.*'],
+  subscribers: ['dist/subscriber/**/*.*'],
+  cli: {
+    entitiesDir: 'dist/models',
+    migrationsDir: 'dist/migration',
+    subscribersDir: 'dist/subscriber',
+  },
+})
   .then(() => {
     console.log('Database connected');
     app.listen(PORT, async () => {

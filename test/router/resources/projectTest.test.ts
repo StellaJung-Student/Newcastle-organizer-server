2aa899e08e8b1caad8215c85134be16f7db6980f
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-11-29 03:48:24 +0100

    Change google auth

--- test/router/resources/projectTest.test.ts
+++
@@ -1,6 +0,0 @@
-import request from 'supertest';
-import app from '../../../server/app';
-
-describe('Test Public project router will render public project', () => {
-  it('Request /api/projects should return public project', async () => {});
-});

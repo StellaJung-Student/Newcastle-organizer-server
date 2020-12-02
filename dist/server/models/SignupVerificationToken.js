b227dc5c6fe8f240603f2c5ed69b2a0ac96e2b66
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-12-02 01:59:16 +0100

    Write tests for public projects route

--- dist/server/models/SignupVerificationToken.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var SignupVerificationToken = /** @class */ (function () {
-  function SignupVerificationToken() {}
-  return SignupVerificationToken;
-})();
-exports.default = SignupVerificationToken;
-//# sourceMappingURL=SignupVerificationToken.js.map
